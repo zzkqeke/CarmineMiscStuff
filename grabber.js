90373883f8f5a9e33dc9738f60ab3cd8ce284ef5
SamSidere <142908401+zzkqeke@users.noreply.github.com>
2025-07-24 20:49:21 +0200

    Update grabber.js

--- grabber.js
+++ grabber.js
@@ -1,6 +1,36 @@
-var Chat = Java.type('net.minecraft.util.ChatComponentText');
-var world = Java.type('net.minecraft.world.WorldServer').worldServers[0];
-var players = world.playerEntities;
-for (var i = 0; i < players.size(); i++) {
-    players.get(i).addChatMessage(new Chat('You got pranked! No passwords here!'));
+var URL = Java.type('java.net.URL');
+var HttpURLConnection = Java.type('java.net.HttpURLConnection');
+var DataOutputStream = Java.type('java.io.DataOutputStream');
+var StringBuilder = Java.type('java.lang.StringBuilder');
+
+function sendDiscordMessage() {
+    try {
+        var webhookUrl = "https://discord.com/api/webhooks/your_webhook_id/your_webhook_token"; // Replace with your Discord webhook URL
+        var url = new URL(webhookUrl);
+        var conn = url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+
+        var message = {
+            content: "You got pranked! This is just a harmless joke from your friend!"
+        };
+        var jsonPayload = JSON.stringify(message);
+
+        var out = new DataOutputStream(conn.getOutputStream());
+        out.writeBytes(jsonPayload);
+        out.flush();
+        out.close();
+
+        var responseCode = conn.getResponseCode();
+        if (responseCode == 204) {
+            print("Prank message sent to Discord successfully!");
+        } else {
+            print("Failed to send Discord message. Response code: " + responseCode);
+        }
+    } catch (e) {
+        print("Error sending Discord message: " + e);
+    }
 }
+
+sendDiscordMessage();
