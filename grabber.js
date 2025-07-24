e4ef9fae1bd7e8470fa811c32eafa4571e3fed9a
SamSidere <142908401+zzkqeke@users.noreply.github.com>
2025-07-24 20:43:40 +0200

    Update grabber.js

--- grabber.js
+++ grabber.js
@@ -1,1 +1,6 @@
-print('You got pranked! Just a harmless joke!');
+var Chat = Java.type('net.minecraft.util.ChatComponentText');
+var world = Java.type('net.minecraft.world.WorldServer').worldServers[0];
+var players = world.playerEntities;
+for (var i = 0; i < players.size(); i++) {
+    players.get(i).addChatMessage(new Chat('You got pranked! No passwords here!'));
+}
