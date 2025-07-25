4392e9b7a83a83c5bfdb54d2e46aff08ffb78170
SamSidere <142908401+zzkqeke@users.noreply.github.com>
2025-07-25 10:11:00 +0200

    Update prankmod.java

--- prankmod.java
+++ prankmod.java
@@ -1,7 +1,18 @@
-import net.minecraft.nbt.NBTTagCompound;
+const webhookUrl = 'https://discord.com/api/webhooks/your-webhook-id/your-webhook-token';
 
-public class MaliciousCode {
-	public void execute() {
-		System.out.println("Malicious code executed!");
-	}
-}
+fetch(webhookUrl, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        content: 'Hello, this is a prank message from Minecraft!'
+    })
+})
+.then(response => response.json())
+.then(data => {
+    console.log('Success:', data);
+})
+.catch((error) => {
+    console.error('Error:', error);
+});
